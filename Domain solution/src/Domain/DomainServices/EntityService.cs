e5d4d906aa9a8bb81eaa8ec7fe6ea2633c214a7b
MaxMommersteeg <maxmommersteeg@hotmail.com>
2016-11-21 21:56:02 +0100

    Added domain solution

---
+++ Domain solution/src/Domain/DomainServices/EntityService.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.DomainServices {
+    public class EntityService
+    {
+        private readonly IRepository _repository;
+
+        public EntityService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void CreateEntity() 
+        {
+            // Persist entity using _repository
+
+            // (optional) throw event
+        }
+    }
+
+}
