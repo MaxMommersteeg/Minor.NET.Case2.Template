40ee46fe8bcd761b3a698ee2ad1847ea19bdde93
Yael Keemink <yaelkeemink@gmail.com>
2016-11-22 22:33:53 +0100

    Changes on template

---
+++ Template/App/Entities/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace $safeprojectname$
+{
+    public class Room
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Running { get; set; }
+        
+        public int WinnerId { get; set; }
+        public Player Winner { get; set; }
+
+        public IList<Player> Players { get; set; }
+    }
+}
