4cffb63d8d6f24c14e019777271b2dc5c3c3a0cf
Jeroen` <Jeroen`@JEROEN-DESK>
2016-11-22 21:51:58 +0100

    Veranderingen aan Controller

---
+++ Case2 template/Backend/Minor.Dag39.GamesBackend/GamesBack.Facade.Controller/FacadeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesBack.Facade.Controller
+{
+    [TestClass]
+    public class FacadeControllerTest
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
---
+++ Case2 template/Backend/Minor.Dag39.GamesBackend/Minor.Dag39.GamesBackend/src/Incoming/Minor.Dag39.GamesBackend.Incoming/Commands/EndGameCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Minor.Dag39.GamesBackend.Incoming.Commands
+{
+    public class EndGameCommand : ICommand
+    {
+        public Guid CommandId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string RoomName { get; set; }
+    }
+}
